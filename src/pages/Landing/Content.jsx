c9baf5e806c963e97eb8f07c307b9924f2c8d864
vendrie yulman <vendriey@gmail.com>
2021-04-29 13:44:39 +0700

    commit content container

--- src/pages/Landing/Content.jsx
+++ src/pages/Landing/Content.jsx
@@ -1,4 +1,13 @@
-import { Layout, Carousel } from 'antd'
+import { Layout, Carousel, Card, Row, Col, Typography } from 'antd'
+import {
+  FileTextOutlined,
+  MessageOutlined,
+  CalendarOutlined,
+  PhoneOutlined,
+  TeamOutlined,
+  ContainerOutlined
+} from '@ant-design/icons'
+import Kegiatan from './Kegiatan'
 
 const { Content } = Layout
 
@@ -10,7 +19,8 @@ const contentStyle = {
   background: '#364d79'
 }
 
-  
+const { Text } = Typography
+
 const ContentContainer = () => {
   return (
     <>
@@ -28,7 +38,61 @@ const ContentContainer = () => {
           <h3 style={contentStyle}>4</h3>
         </div>
       </Carousel>
-      <Content>Content</Content>
+      <Content>
+        <div style={{ backgroundColor: '#fff' }}>
+          <Row justify="center">
+            {[
+              {
+                title: 'Mahasiswa Baru',
+                link: '',
+                icon: <FileTextOutlined style={{ fontSize: 60 }} />
+              },
+              {
+                title: 'Portal Mahasiswa',
+                link: '',
+                icon: <MessageOutlined style={{ fontSize: 60 }} />
+
+              },
+              {
+                title: 'Kalender Akademik',
+                link: '',
+                icon: <CalendarOutlined style={{ fontSize: 60 }} />
+              },
+              {
+                title: 'Hubungi Kami',
+                link: '',
+                icon: <PhoneOutlined style={{ fontSize: 60 }} />
+
+              },
+              {
+                title: 'Lowongan Kerja',
+                link: '',
+                icon: <ContainerOutlined style={{ fontSize: 60 }} />
+
+              },
+              {
+                title: 'Ikatan Alumni',
+                link: '',
+                icon: <TeamOutlined style={{ fontSize: 60 }} />
+              }
+            ].map((item, i) => {
+              return (
+                <Col key={i}>
+                  <Card bodyStyle={{ padding: '0.5em 24px' }} style={{ margin: '0.5em', width: 150 }}>
+                    <Row justify="center">
+                      {item.icon}
+                    </Row>
+                    <Row justify="center" style={{ marginTop: '1em', textAlign: 'center' }}>
+                      <Text strong style={{ fontSize: 18 }}>{item.title}</Text>
+                    </Row>
+                  </Card>
+                </Col>
+              )
+            })}
+          </Row>
+        </div>
+        <Kegiatan />
+      </Content>
     </>
   )
 }
