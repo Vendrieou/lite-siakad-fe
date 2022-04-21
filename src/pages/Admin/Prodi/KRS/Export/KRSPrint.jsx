f24736e87e9de3aa4b056fe6a68e05c30a54475d
vendrie <vendrieok@gmail.com>
2022-04-21 22:38:20 +0700

    commit ccustom hook to print pdf

--- src/pages/Admin/Prodi/KRS/Export/KRSPrint.jsx
+++ src/pages/Admin/Prodi/KRS/Export/KRSPrint.jsx
@@ -1,63 +1,15 @@
-import { useState, useRef, useEffect, useCallback } from 'react'
-import { useReactToPrint } from 'react-to-print'
+import usePrintPdf from 'components/hooks/usePrintPdf'
 import { Button } from 'antd'
 
 import ComponentToPrint from './ComponentToPrint'
 
 const KRSPrint = () => {
-  const componentRef = useRef(null)
-
-  const onBeforeGetContentResolve = useRef(null)
-
-  const [loading, setLoading] = useState(false)
-  const [text, setText] = useState('old boring text')
-
-  const handleAfterPrint = useCallback(() => {
-    console.log('`onAfterPrint` called') // tslint:disable-line no-console
-  }, [])
-
-  const handleBeforePrint = useCallback(() => {
-    console.log('`onBeforePrint` called') // tslint:disable-line no-console
-  }, [])
-
-  const handleOnBeforeGetContent = useCallback(() => {
-    console.log('`onBeforeGetContent` called') // tslint:disable-line no-console
-    setLoading(true)
-    setText('Loading new text...')
-
-    return new Promise((resolve) => {
-      onBeforeGetContentResolve.current = resolve
-
-      setTimeout(() => {
-        setLoading(false)
-        setText('New, Updated Text!')
-        resolve()
-      }, 2000)
-    })
-  }, [setLoading, setText])
-
-  const reactToPrintContent = useCallback(() => {
-    return componentRef.current
-  }, [componentRef.current])
-
-  const handlePrint = useReactToPrint({
-    content: reactToPrintContent,
-    documentTitle: 'AwesomeFileName',
-    onBeforeGetContent: handleOnBeforeGetContent,
-    onBeforePrint: handleBeforePrint,
-    onAfterPrint: handleAfterPrint,
-    removeAfterPrint: true
-  })
-
-  useEffect(() => {
-    if (
-      text === 'New, Updated Text!' &&
-      typeof onBeforeGetContentResolve.current === 'function'
-    ) {
-      onBeforeGetContentResolve.current()
-    }
-  }, [onBeforeGetContentResolve.current, text])
-
+  const { 
+    handlePrint,
+    componentRef,
+    text,
+    loading
+  } = usePrintPdf({ documentTitle: 'KRS' })
 
   const data = [
     {
@@ -99,7 +51,7 @@ const KRSPrint = () => {
 
   return (
     <div>
-      {loading && <p className='indicator'>onBeforeGetContent: Loading...</p>}
+      {loading && <p className='indicator'>Loading...</p>}
       <Button type="danger" onClick={handlePrint}>
         Print
       </Button>
