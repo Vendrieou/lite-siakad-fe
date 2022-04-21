c2e916d682285a78bfdbdfbc93cd73952fd381f5
vendrie01 <vendriey@gmail.com>
2022-04-21 16:25:19 +0700

    fixing KRS prodi search

--- src/pages/Admin/Prodi/KRS/Export/ComponentToPrint.jsx
+++ src/pages/Admin/Prodi/KRS/Export/ComponentToPrint.jsx
@@ -1,121 +1,94 @@
-import { PureComponent } from 'react'
+import { Typography, Table }  from 'antd'
 import logoTIME from 'static/logo/bwLogoTime.png'
 import styles from './ComponentToPrint.module.less'
+import { dateTimeToFull } from 'utils/time'
+
+const { Title } = Typography
+const columns = [
+  {
+    title: 'Kode MK',
+    dataIndex: 'kodeMK',
+    key: 'name',
+    width: 200,
+  },
+  {
+    title: 'Mata Kuliah',
+    dataIndex: 'matkul',
+    key: 'name',
+    width: 400,
+  },
+  {
+    title: 'SKS',
+    dataIndex: 'sks',
+    key: 'name',
+    width: 20,
+  },
+  {
+    title: 'Semester',
+    dataIndex: 'semester',
+    key: 'name',
+    width: 20,
+  },
+  {
+    title: 'Kelas',
+    dataIndex: 'kelas',
+    key: 'name',
+    width: 150,
+  },
+  {
+    title: 'Nama Dosen',
+    dataIndex: 'namaDosen',
+    key: 'name',
+    width: 600,
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    kodeMK: 'TI 2639',
+    matkul: 'PRAKTIKUM STATISTIKA',
+    sks: 1,
+    semester: 6,
+    kelas: 'TI C 18',
+    namaDosen: 'EMMA ROSINTA, S.SI, S.KOM, M.KOM',
+  },
+  {
+    key: '2',
+    kodeMK: 'TI 2639',
+    matkul: 'PRAKTIKUM 3D MODELING',
+    sks: 1,
+    semester: 6,
+    kelas: 'TI C 18',
+    namaDosen: 'JOHANNES TERANG KITA PERANGIN ANGIN',
+  },
+]
+
+const assignData = {
+  kaProdi: 'Robert, M.KOM',
+  name: 'VENDRIE YULMAN',
+  nim: '1844017',
+  kelas: 'TI C 18',
+  tahunAjaran: '2021 - 2022',
+  programStudi: 'TI',
+  semester: '6',
+  dosenWali: 'Didik Aryanto',
+}
 
 class ComponentToPrint extends React.PureComponent {
   render() {
     return (
-      <div className={styles['container']}>
+      <div>
         <div className={styles['group24566519768']}>
           <div className={styles['group24466519770']}>
-            <span className={styles['text']}>Jumlah</span>
-            <span className={styles['text001']}>18</span>
-         
-            <div className={styles['group23666519788']}>
-              <span className={styles['text002']}>TI PR 0606</span>
-              <span className={styles['text003']}>TI 6603</span>
-              <span className={styles['text004']}>TI 2639</span>
-              <span className={styles['text005']}>TI 4605</span>
-              <span className={styles['text006']}>TI 2638</span>
-              <span className={styles['text007']}>TI 2641</span>
-              <span className={styles['text008']}>TI 2640</span>
-              <span className={styles['text009']}>TI 4606</span>
-              <span className={styles['text010']}>Kode MK</span>
-            </div>
-            <div className={styles['group23866519798']}>
-              <span className={styles['text011']}>6</span>
-              <span className={styles['text012']}>6</span>
-              <span className={styles['text013']}>6</span>
-              <span className={styles['text014']}>6</span>
-              <span className={styles['text015']}>6</span>
-              <span className={styles['text016']}>6</span>
-              <span className={styles['text017']}>6</span>
-              <span className={styles['text018']}>6</span>
-              <span className={styles['text019']}>Semester</span>
-            </div>
-            <div className={styles['group24166519808']}>
-              <span className={styles['text020']}>TI C 18</span>
-              <span className={styles['text021']}>TI C 18</span>
-              <span className={styles['text022']}>TI C 18</span>
-              <span className={styles['text023']}>TI C 18</span>
-              <span className={styles['text024']}>TI C 18</span>
-              <span className={styles['text025']}>TI C 18</span>
-              <span className={styles['text026']}>TI C 18</span>
-              <span className={styles['text027']}>TI C 18</span>
-              <span className={styles['text028']}>Kelas</span>
-            </div>
-            <div className={styles['group24266519818']}>
-              <span className={styles['text029']}>
-                EMMA ROSINTA, S.SI, S.KOM, M.KOM
-              </span>
-              <span className={styles['text030']}>JACKRI HENDRIK, ST, M.KOM</span>
-              <span className={styles['text031']}>
-                DR. N.S. SIHOMBING, ST, S.KOM, MM
-              </span>
-              <span className={styles['text032']}>
-                JOHANNES TERANG GINGING, M.KOM.
-              </span>
-              <span className={styles['text033']}>
-                EMMA ROSINTA, S.SI, S.KOM, M.KOM
-              </span>
-              <span className={styles['text034']}>
-                TONY RIA, S.PD, S.KOM, M.KOM
-              </span>
-              <span className={styles['text035']}>
-                TONY RIA, S.PD, S.KOM, M.KOM
-              </span>
-              <span className={styles['text036']}>
-                JOHANNES TERANG GINGING, M.KOM.
-              </span>
-              <span className={styles['text037']}>Nama Dosen</span>
-            </div>
-            <div className={styles['group23966519828']}>
-              <span className={styles['text038']}>3</span>
-              <span className={styles['text039']}>3</span>
-              <span className={styles['text040']}>1</span>
-              <span className={styles['text041']}>3</span>
-              <span className={styles['text042']}>3</span>
-              <span className={styles['text043']}>1</span>
-              <span className={styles['text044']}>3</span>
-              <span className={styles['text045']}>1</span>
-              <span className={styles['text046']}>SKS</span>
-            </div>
-            <div className={styles['group24066519838']}>
-              <span className={styles['text047']}>2</span>
-              <span className={styles['text048']}>3</span>
-              <span className={styles['text049']}>1</span>
-              <span className={styles['text050']}>4</span>
-              <span className={styles['text051']}>5</span>
-              <span className={styles['text052']}>6</span>
-              <span className={styles['text053']}>7</span>
-              <span className={styles['text054']}>8</span>
-              <span className={styles['text055']}>No</span>
-            </div>
-            <div className={styles['group23766519848']}>
-              <span className={styles['text056']}>METODE PENELITIAN</span>
-              <span className={styles['text057']}>
-                PENGEMBANGAN KEPRIBADIAN DIRI
-              </span>
-              <span className={styles['text058']}>PRAKTIKUM STATISTIKA</span>
-              <span className={styles['text059']}>3D MODELING</span>
-              <span className={styles['text060']}>STATISTIKA</span>
-              <span className={styles['text061']}>
-                PRAKTIKUM MODEL DAN SIMULASI
-              </span>
-              <span className={styles['text062']}>MODEL DAN SIMULASI</span>
-              <span className={styles['text063']}>PRAKTIKUM 3D MODELING</span>
-              <span className={styles['text064']}>Mata Kuliah</span>
-            </div>
-            <span className={styles['text065']}>
-              <span>Nama Mahasiswa : VENDRIE YULMAN</span>
-              <br></br>
-              <span>NIM : 1844017</span>
-              <br></br>
-              <span>Prg Studi : TI</span>
-              <br></br>
-              <span>Semester : VI</span>
+
+          <span className={styles['text065']}>
+              <Title level={5} style={{ margin: 0 }}>Nama : {assignData.name}</Title>
+              <Title level={5} style={{ margin: 0 }}>NIM : {assignData.nim}</Title>
+              <Title level={5} style={{ margin: 0 }}>Program Studi : {assignData.programStudi}</Title>
+              <Title level={5} style={{ margin: 0 }}>Semester : {assignData.semester}</Title>
             </span>
-            <span className={styles['text073']}>Kelas : TI C 18</span>
+            <Title level={5} style={{ margin: 0 }} className={styles['text073']}>Kelas : {assignData.kelas}</Title>
             <span className={styles['text074']}>
               <span>
                 SEKOLAH TINGGI MANAJEMEN INFORMATIKA &amp; KOMPUTER - TIME
@@ -123,23 +96,21 @@ class ComponentToPrint extends React.PureComponent {
               <br></br>
               <span>Jl. Merbabu No. 32 AA - BB. Telp 061 - 4561932, 4533676</span>
               <br></br>
-              <span>KARTU RENCANA STUDI</span>
+              <Title>KARTU RENCANA STUDI</Title>
               <br></br>
-              <span>TA. 2021 - 2022</span>
+              <span>TA. {assignData.tahunAjaran}</span>
             </span>
             <img
               src={logoTIME}
               alt="logoTime166519861"
               className={styles['image15']}
             />
+            <Table dataSource={data} columns={columns} pagination={false} className={styles['groupTable']} />
           </div>
           <div className={styles['group24366519862']}>
             <span className={styles['text082']}>
-              <span>Medan, 08 Februari 2021</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span>Mahasiswa</span>
+              <span>Medan, {dateTimeToFull(new Date())}</span>
+              <p>Mahasiswa</p>
               <br></br>
               <span></span>
               <br></br>
@@ -155,14 +126,11 @@ class ComponentToPrint extends React.PureComponent {
               <br></br>
               <span></span>
               <br></br>
-              <span>(VENDRIE YULMAN)</span>
+              <span>{`(${assignData.name})`}</span>
             </span>
             <span className={styles['text104']}>
               <span>Disetujui</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span>Dosen Wali</span>
+              <p>Dosen Wali</p>
               <br></br>
               <span></span>
               <br></br>
@@ -178,7 +146,7 @@ class ComponentToPrint extends React.PureComponent {
               <br></br>
               <span></span>
               <br></br>
-              <span>(DIDIK ARYANTO, S.KOM, M.KOM)</span>
+              <span>{`(${assignData.dosenWali})`}</span>
               <br></br>
               <span></span>
               <br></br>
@@ -186,10 +154,7 @@ class ComponentToPrint extends React.PureComponent {
             </span>
             <span className={styles['text130']}>
               <span>Diketahui</span>
-              <br></br>
-              <span></span>
-              <br></br>
-              <span>Ka. Prodi</span>
+              <p>Ka. Prodi</p>
               <br></br>
               <span></span>
               <br></br>
@@ -205,7 +170,7 @@ class ComponentToPrint extends React.PureComponent {
               <br></br>
               <span></span>
               <br></br>
-              <span>(Robert, M.KOM)</span>
+              <span>{`(${assignData.kaProdi})`}</span>
             </span>
           </div>
         </div>
@@ -215,110 +180,3 @@ class ComponentToPrint extends React.PureComponent {
 }
 
 export default ComponentToPrint
-
-// return (
-//   <div className="relativeCSS">
-//     <style type="text/css" media="print">
-//       {"\
-//         @page { size: landscape; }\
-//       "}
-//     </style>
-//     <div className="flash" />
-//     <table className="testClass">
-//       <thead>
-//         <tr>
-//           <th className="column1">Test Name</th>
-//           <th>Test</th>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         <tr>
-//           <td>Fonts</td>
-//           <td>
-//             <div className="customFontText">Some Cool Font Text</div>
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Image: Local Import</td>
-//           <td>
-//             <img alt="A test" src={logoTIME} width="200" />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Image: URL</td>
-//           <td>
-//             <img
-//               alt="Google logo"
-//               src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-//               width="200"
-//             />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Input</td>
-//           <td>
-//             <input />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Input: Date</td>
-//           <td>
-//             <input type="date" />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Input: Radio</td>
-//           <td>
-//             Blue <input type="radio" id="blue" name="color" value="blue" />
-//             Red <input type="radio" id="red" name="color" value="red" />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Select</td>
-//           <td>
-//             <select name="cars" id="cars">
-//               <option value="volvo">Volvo</option>
-//               <option value="saab">Saab</option>
-//               <option value="mercedes">Mercedes</option>
-//               <option value="audi">Audi</option>
-//             </select>
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>SVG</td>
-//           <td>
-//             <svg height="100" width="100">
-//               <circle
-//                 cx="50"
-//                 cy="50"
-//                 fill="yellow"
-//                 r="40"
-//                 stroke="green"
-//                 strokeWidth="4"
-//               />
-//             </svg>
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Video</td>
-//           <td>
-//             <video
-//               src="https://www.w3schools.com/html/mov_bbb.mp4"
-//               width="200"
-//             />
-//           </td>
-//         </tr>
-//         <tr>
-//           <td>Video: With Poster</td>
-//           <td>
-//             <video
-//               poster="https://images.freeimages.com/images/large-previews/9a9/tuscany-landscape-4-1500765.jpg"
-//               src="https://www.w3schools.com/html/mov_bbb.mp4"
-//               width="200"
-//             />
-//           </td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   </div>
-// )
